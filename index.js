c3d6238fa0cf8c3e2ea752fe0080db248f11eece
sheamZero <sheamshadik078@gmail.com>
2025-06-16 16:37:41 +0600

    add api to update existing item

--- index.js
+++ index.js
@@ -34,12 +34,12 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
-        
+
         // specific item
         app.get("/items/:id", async (req, res) => {
             const id = req.params.id;
-            const new_id = { _id: new ObjectId(id) };
-            const result = await craftItems.findOne(new_id);
+            const cursor = { _id: new ObjectId(id) };
+            const result = await craftItems.findOne(cursor);
             res.send(result);
         })
 
@@ -49,6 +49,28 @@ async function run() {
             const result = await craftItems.insertOne(newData);
             res.send(result);
             console.log(newData);
+        })
+
+        // update items
+        app.put("/items/:id", async (req, res) => {
+            const newItem = req.body;
+            const filter = { _id: new ObjectId(req.params.id) };
+            const option = { upsert: true }
+            const updatedItem = {
+                $set: {
+                    image: newItem.image,
+                    item_name: newItem.item_name,
+                    subcategory_name: newItem.subcategory_name,
+                    price: newItem.price,
+                    rating: newItem.rating,
+                    customization: newItem.customization,
+                    processing_time: newItem.processing_time,
+                    stockStatus: newItem.stockStatus,
+                    short_description: newItem.short_description,
+                }
+            }
+            const result = await craftItems.updateOne(filter, updatedItem, option);
+            res.send(result);
         })
 
 
