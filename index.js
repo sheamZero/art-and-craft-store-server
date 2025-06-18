8e26f31ef3d59a3b24a9c7adfc0ac5b873174d1b
sheamZero <sheamshadik078@gmail.com>
2025-06-18 12:49:39 +0600

    add env variable

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gusrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
     serverApi: {
